4c75d97bb02d266c51ac70c6f9bfa13f3a54c2ce
keseter <worang19@gmail.com>
2026-02-21 14:24:40 +0700

    Added a home landing page and increase unit test to 95%.

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void homePage_rendersHomePageWithGoToProductListButton() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(org.hamcrest.Matchers.containsString("Welcome")))
+                .andExpect(content().string(org.hamcrest.Matchers.containsString("Go to Product List")))
+                .andExpect(content().string(org.hamcrest.Matchers.containsString("/product/list")));
+    }
+}
