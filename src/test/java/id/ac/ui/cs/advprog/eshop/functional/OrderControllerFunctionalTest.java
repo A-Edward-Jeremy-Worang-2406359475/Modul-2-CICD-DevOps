2ff676261a9d0d532598856eec2cb013a9f4a48d
keseter <worang19@gmail.com>
2026-03-10 17:04:57 +0700

    [RED] Add functional tests for Order feature

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/OrderControllerFunctionalTest.java
@@ -0,0 +1,53 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import id.ac.ui.cs.advprog.eshop.service.OrderService;
+import id.ac.ui.cs.advprog.eshop.service.PaymentService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import id.ac.ui.cs.advprog.eshop.controller.OrderController;
+
+@WebMvcTest(OrderController.class)
+class OrderControllerFunctionalTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OrderService orderService;
+
+    @MockBean
+    private PaymentService paymentService;
+
+    @Test
+    @DisplayName("GET /order/create should display create order page")
+    void testGetCreateOrderPage() throws Exception {
+        mockMvc.perform(get("/order/create"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("createOrder"));
+    }
+
+    @Test
+    @DisplayName("GET /order/history should display order history input form")
+    void testGetOrderHistoryForm() throws Exception {
+        mockMvc.perform(get("/order/history"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("orderHistoryForm"));
+    }
+
+    @Test
+    @DisplayName("GET /order/pay/{orderId} should display payment page")
+    void testGetPayOrderPage() throws Exception {
+        mockMvc.perform(get("/order/pay/order-123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("payOrder"))
+                .andExpect(model().attributeExists("orderId"));
+    }
+}
